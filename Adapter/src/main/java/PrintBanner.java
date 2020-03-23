1506ead17315968754023b2a47fc4586ba8d712d
salthash329 <salt.hash329@gmail.com>
2020-03-23 22:50:03 +0900

    Adapter

---
+++ Adapter/src/main/java/PrintBanner.java
@@ -0,0 +1,16 @@
+public class PrintBanner extends Banner implements Print{
+
+    public PrintBanner(String string){
+        super((string));
+    }
+
+    public void printWeak(){
+        showWithParen();
+    }
+
+    public void printStrong(){
+        showWithAster();
+    }
+
+
+}
