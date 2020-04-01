5e7ec102f597cb0413a902f4d8ba8bf42ae04912
salthash329 <salt.hash329@gmail.com>
2020-04-01 21:26:07 +0900

    Main,class

--- TemplateMethod/src/main/java/Main.java
+++ TemplateMethod/src/main/java/Main.java
@@ -5,7 +5,6 @@ public class Main {
         AbstractDisplay d1 = new CharDisplay('H');
         AbstractDisplay d2 = new StringDisplay("Hello, World");
         AbstractDisplay d3 = new StringDisplay("こんにちは。");
-
         
         d1.display();
         d2.display();
